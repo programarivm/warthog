85d52c88a63a7bd8e67eb3d557388ede5238821e
programarivm <info@programarivm.com>
2020-01-11 14:28:45 +0000

    session cleanup

--- resources/js/Warthog.js
+++ resources/js/Warthog.js
@@ -1,23 +1,14 @@
-import ability from './ability';
-import abilityRules from '../../storage/ability-rules.json';
 import { connect } from 'react-redux';
 import PrivateApp from './components/private/App';
 import PublicApp from './components/public/App';
 import React, { Component } from 'react';
-import Session from './Session';
 import './Warthog.css';
 
 class Warthog extends Component {
-  componentDidMount() {
-    if (Session.get()) {
-      ability.update(abilityRules[Session.get().role]);
-    }
-  }
-
   render() {
     return (
       <div className="Warthog">
-        { this.props.session.role || Session.get().role ? <PrivateApp /> : <PublicApp /> }
+        { this.props.session.role ? <PrivateApp /> : <PublicApp /> }
       </div>
     );
   }
