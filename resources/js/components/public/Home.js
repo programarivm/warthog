a933d92c15219d928f90641e5f0163e2091f0c74
programarivm <info@programarivm.com>
2020-03-29 16:43:36 +0100

    reactstrap has been replaced with material-ui in components/public/Home.js

--- resources/js/components/public/Home.js
+++ resources/js/components/public/Home.js
@@ -1,42 +1,33 @@
-import { Col, Container, Row } from 'reactstrap';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid'
 import { Footer } from './Footer';
 import { Link } from 'react-router-dom';
 import React from 'react';
-import api from '../../../images/api.png';
 import './Home.css';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <Container className="Home mt-5 mb-5">
-          <Row>
-            <Col lg="8">
-              <h1 className="mt-5">Welcome to Warthog</h1>
-              <h5>This is a real-world example SPA: a React GUI interacting with a Laravel API.</h5>
-              <hr/>
-              <p>Features:</p>
-              <ul>
-                <li>ACL (access control list)</li>
-                <li>JWT authentication</li>
-                <li>CRUD implementation</li>
-                <li>REST API</li>
-                <li>Redux</li>
-                <li>Data-driven tests</li>
-              </ul>
-              <Link className="mt-2 btn btn-primary text-white" to="/login">Interact now!</Link>
-            </Col>
-            <Col lg="4">
-              <figure className="swing">
-                <img src={api} className="img-fluid" alt="soup" />
-              </figure>
-            </Col>
-          </Row>
-        </Container>
-        <Footer />
-      </div>
-    );
-  }
+function Home() {
+  return (
+    <Grid container style={{ margin: 20, padding: 20 }}>
+      <Grid item xs={8}>
+        <h1>Welcome to Warthog</h1>
+        <h5>This is a real-world example SPA: a React GUI interacting with a Laravel API.</h5>
+        <hr/>
+        <p>Features:</p>
+        <ul>
+          <li>ACL (access control list)</li>
+          <li>JWT authentication</li>
+          <li>CRUD implementation</li>
+          <li>REST API</li>
+          <li>Redux</li>
+          <li>Data-driven tests</li>
+        </ul>
+        <Button variant="contained" color="primary" component={Link} to={'/login'}>Interact now!</Button>
+      </Grid>
+      <Grid item xs={4}>
+        <p>TODO: Display image</p>
+      </Grid>
+    </Grid>
+  );
 }
 
 export { Home };
