c91b961da2ec111568f0768e03b93cdc8869bff4
programarivm <info@programarivm.com>
2020-04-05 13:03:07 +0100

    reactstrap is replaced with material-ui in the ReviewIndex component

--- resources/js/components/common/review/Index.js
+++ resources/js/components/common/review/Index.js
@@ -1,15 +1,31 @@
 import ApiReviewActions from '../../../actions/api/ReviewActions';
-import { Button, ButtonGroup, Container } from 'reactstrap';
+import { Button, ButtonGroup, Grid, IconButton, Paper, Table, TableBody, TableCell,
+  TableContainer, TableFooter, TableHead, TablePagination, TableRow } from '@material-ui/core';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Can from '../../Can';
 import { connect } from 'react-redux';
 import Loading from '../../Loading';
 import { LoremIpsum } from '../LoremIpsum';
 import React from 'react';
-import ReactTable from 'react-table';
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
 
 class ReviewIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      page: 0,
+      rowsPerPage: 25
+    };
     this.handleClickDelete = this.handleClickDelete.bind(this);
   }
 
@@ -27,72 +43,116 @@ class ReviewIndex extends React.Component {
     e.preventDefault();
   }
 
-  render() {
-    const columns = [
-      {
-        Header: 'Created at',
-        accessor: 'created_at',
-        Cell: ({ row }) => {
-          let d = new Date(row._original.created_at);
-          return `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`;
-        }
-      },
-      {
-        Header: 'User',
-        accessor: 'user',
-        Cell: ({ row }) => (
-          `${row._original.user.firstname}`
-        )
-      },
-      {
-        Header: 'Restaurant',
-        accessor: 'restaurant',
-        Cell: ({ row }) => (
-          `${row._original.restaurant.name}`
-        )
-      },
-      {
-        Header: 'Comment',
-        accessor: 'comment'
-      },
-      {
-        Header: 'Points',
-        accessor: 'points'
-      }
-    ];
+  handleChangePage = (e, newPage) => {
+    this.setState({ page: newPage });
+  };
 
-    const roleEditorColumns = [
-      ...columns,
-      {
-        Header: 'Actions',
-        accessor: 'actions',
-        Cell: ({ row }) => (
-          <ButtonGroup>
-            <Button outline color="primary" size="sm" onClick={ (e) => this.handleClickDelete(e,row._original.id) }>Delete</Button>
-          </ButtonGroup>
-        )
-      }
-    ];
+  tablePaginationActions(props) {
+    const theme = useTheme();
+
+    const classes = useStyles1();
+
+    const { count, page, rowsPerPage, onChangePage } = props;
+
+    const handleFirstPageButtonClick = (event) => {
+      onChangePage(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+      onChangePage(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+      onChangePage(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+      onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
 
     return (
-      <Container className="m-3">
+      <div className={classes.root}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Grid container>
         <Loading loading={this.props.loading}>
-          <Can I="delete" a="Review">
-            <ReactTable
-              data={this.props.data}
-              columns={roleEditorColumns}
-              minRows={0}
-            />
-          </Can>
-          <Can not I="delete" a="Review">
-            <ReactTable
-              data={this.props.data}
-              columns={columns}
-              minRows={0}
-            />
-          </Can>
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell></TableCell>
+                    <TableCell>User</TableCell>
+                    <TableCell>Restaurant</TableCell>
+                    <TableCell>Comment</TableCell>
+                    <TableCell align="right">Points</TableCell>
+                    <Can I="delete" a="Review">
+                      <TableCell>Actions</TableCell>
+                    </Can>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.props.data ? this.props.data.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map((row, i) => {
+                    let d = new Date(row.created_at);
+                    return <TableRow key={i}>
+                      <TableCell align="right">{d.getDate()}/{d.getMonth()+1}/{d.getFullYear()}</TableCell>
+                      <TableCell>{row.user.firstname}</TableCell>
+                      <TableCell>{row.restaurant.name}</TableCell>
+                      <TableCell>{row.comment}</TableCell>
+                      <TableCell align="right">{row.points}</TableCell>
+                      <Can I="delete" a="Review">
+                        <TableCell align="right">
+                          <ButtonGroup color="primary" size="small" aria-label="outlined primary button group">
+                            <Button onClick={ (e) => this.handleClickDelete(e, row.id) }>Delete</Button>
+                          </ButtonGroup>
+                        </TableCell>
+                      </Can>
+                    </TableRow>
+                  }) : null }
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[]}
+                      rowsPerPage={this.state.rowsPerPage}
+                      count={this.props.data ? this.props.data.length : 0}
+                      page={this.state.page}
+                      onChangePage={this.handleChangePage}
+                      ActionsComponent={this.tablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </TableContainer>
         </Loading>
-      </Container>
+      </Grid>
     );
   }
 }
