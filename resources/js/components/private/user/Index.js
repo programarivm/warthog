e24ecd3862976321a604c4f8d5f7ae0042634128
programarivm <info@programarivm.com>
2020-04-10 09:40:36 +0100

    reactstrap is replaced with material-ui in the UserIndex component

--- resources/js/components/private/user/Index.js
+++ resources/js/components/private/user/Index.js
@@ -1,15 +1,31 @@
 import ApiUserActions from '../../../actions/api/UserActions';
-import { Button, ButtonGroup, Container } from 'reactstrap';
+import { Button, ButtonGroup, Grid, IconButton, Paper, Table, TableBody, TableCell,
+  TableContainer, TableFooter, TableHead, TablePagination, TableRow } from '@material-ui/core';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 import Can from '../../Can';
 import { connect } from 'react-redux';
 import Loading from '../../Loading';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import React from 'react';
-import ReactTable from 'react-table';
 import UserEdit from './Edit';
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
 
 class UserIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      page: 0,
+      rowsPerPage: 25
+    };
     this.handleClickDelete = this.handleClickDelete.bind(this);
     this.handleClickEdit = this.handleClickEdit.bind(this);
   }
@@ -33,70 +49,124 @@ class UserIndex extends React.Component {
     e.preventDefault();
   }
 
-  render() {
-    const columns = [
-      {
-        Header: 'Created at',
-        accessor: 'created_at'
-      },
-      {
-        Header: 'First name',
-        accessor: 'firstname'
-      },
-      {
-        Header: 'Surname',
-        accessor: 'surname'
-      },
-      {
-        Header: 'Date of birth',
-        accessor: 'date_of_birth'
-      },
-      {
-        Header: 'Phone number',
-        accessor: 'phone_number'
-      },
-      {
-        Header: 'Email',
-        accessor: 'email'
-      }
-    ];
+  handleChangePage = (e, newPage) => {
+    this.setState({ page: newPage });
+  };
 
-    const roleAdminColumns = [
-      ...columns,
-      {
-        Header: 'Actions',
-        accessor: 'actions',
-        Cell: ({ row }) => (
-          <ButtonGroup>
-            <Button outline color="primary" size="sm" onClick={ (e) => this.handleClickEdit(e,row._original.id) }>Edit</Button>
-            <Button outline color="primary" size="sm" onClick={ (e) => this.handleClickDelete(e,row._original.id) }>Delete</Button>
-          </ButtonGroup>
-        )
-      }
-    ];
+  tablePaginationActions(props) {
+    const theme = useTheme();
+
+    const classes = useStyles1();
+
+    const { count, page, rowsPerPage, onChangePage } = props;
+
+    const handleFirstPageButtonClick = (event) => {
+      onChangePage(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+      onChangePage(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+      onChangePage(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+      onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
 
     return (
-      <Container className="m-3">
+      <div className={classes.root}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Grid container>
         <Loading loading={this.props.ApiUserFetchAllReducer.loading}>
-          <div>
-            <Can I="store" a="User">
-              <ReactTable
-                data={this.props.ApiUserFetchAllReducer.data}
-                columns={roleAdminColumns}
-                minRows={0}
-              />
-            </Can>
-            <Can not I="store" a="User">
-              <ReactTable
-                data={this.props.ApiUserFetchAllReducer.data}
-                columns={columns}
-                minRows={0}
-              />
-            </Can>
-          </div>
+            <TableContainer component={Paper}>
+              <Table aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="right">Created at</TableCell>
+                    <TableCell>First name</TableCell>
+                    <TableCell>Surname</TableCell>
+                    <TableCell>Date of birth</TableCell>
+                    <TableCell>Phone number</TableCell>
+                    <TableCell>Email</TableCell>
+                    <Can I="store" a="User">
+                      <TableCell>Actions</TableCell>
+                    </Can>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {
+                    this.props.ApiUserFetchAllReducer.data
+                      ? this.props.ApiUserFetchAllReducer.data.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map((row, i) => {
+                          let d = new Date(row.created_at);
+                          return <TableRow key={i}>
+                            <TableCell align="right">{d.getDate()}/{d.getMonth()+1}/{d.getFullYear()}</TableCell>
+                            <TableCell>{row.firstname}</TableCell>
+                            <TableCell>{row.surname}</TableCell>
+                            <TableCell>{row.date_of_birth}</TableCell>
+                            <TableCell>{row.phone_number}</TableCell>
+                            <TableCell>{row.email}</TableCell>
+                            <Can I="store" a="User">
+                              <TableCell align="right">
+                                <ButtonGroup color="primary" size="small" aria-label="outlined primary button group">
+                                  <Button onClick={ (e) => this.handleClickEdit(e, row.id) }>Edit</Button>
+                                  <Button onClick={ (e) => this.handleClickDelete(e, row.id) }>Delete</Button>
+                                </ButtonGroup>
+                              </TableCell>
+                            </Can>
+                          </TableRow>
+                        })
+                      : null
+                    }
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[]}
+                      rowsPerPage={this.state.rowsPerPage}
+                      count={this.props.ApiUserFetchAllReducer.data ? this.props.ApiUserFetchAllReducer.data.length : 0}
+                      page={this.state.page}
+                      onChangePage={this.handleChangePage}
+                      ActionsComponent={this.tablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </TableContainer>
         </Loading>
         <UserEdit />
-      </Container>
+      </Grid>
     );
   }
 }
