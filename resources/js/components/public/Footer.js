93acff2b94988098984cf3f302aad0037fbcc665
programarivm <info@programarivm.com>
2020-04-25 09:54:10 +0100

    playing around with more Material UI components to design the homepage

--- resources/js/components/public/Footer.js
+++ resources/js/components/public/Footer.js
@@ -7,65 +7,75 @@ import LockIcon from '@material-ui/icons/LockTwoTone';
 import SubjectIcon from '@material-ui/icons/SubjectTwoTone';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import YouTubeIcon from '@material-ui/icons/YouTube';
+import Typography from '@material-ui/core/Typography';
 
 function Footer() {
   return (
-    <Grid container style={{ marginTop: 90, padding: 25, backgroundColor: '#e8e8e8' }}>
-      <Grid item xs={4}>
-        <p>
-          <b>Warthog</b><br/>
-          Company Name Goes Here Limited,<br/>
-          Registered in England and Wales,<br/>
-          Company Number: 1234567890
-        </p>
+    <Grid container style={{
+        marginTop: 90,
+        paddingTop: 35,
+        paddingBottom: 35,
+        backgroundColor: '#e8e8e8'
+      }}
+    >
+      <Grid container xs={6} style={{ paddingTop: 35 }}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <Typography color="secondary" variant="subtitle2">
+            Company Name Goes Here Limited,<br/>
+            Registered in England and Wales,<br/>
+            Company Number: 1234567890
+          </Typography>
+        </Grid>
+        <Grid item xs={3}></Grid>
       </Grid>
-      <Grid item xs={4}>
+      <Grid container xs={3}>
         <List component="nav" aria-label="legal">
           <ListItem button>
             <ListItemIcon>
               <SubjectIcon />
             </ListItemIcon>
-            <ListItemText primary="Terms and Conditions" />
+            <ListItemText secondary="Terms and Conditions" />
           </ListItem>
           <ListItem button>
             <ListItemIcon>
               <LockIcon />
             </ListItemIcon>
-            <ListItemText primary="Privacy Policy" />
+            <ListItemText secondary="Privacy Policy" />
           </ListItem>
           <ListItem button>
             <ListItemIcon>
               <EmojiFoodBeverage />
             </ListItemIcon>
-            <ListItemText primary="Cookies Policy" />
+            <ListItemText secondary="Cookies Policy" />
           </ListItem>
         </List>
       </Grid>
-      <Grid item xs={4}>
+      <Grid container xs={3}>
         <List component="nav" aria-label="social">
           <ListItem button>
             <ListItemIcon>
               <TwitterIcon />
             </ListItemIcon>
-            <ListItemText primary="Twitter" />
+            <ListItemText secondary="Twitter" />
           </ListItem>
           <ListItem button>
             <ListItemIcon>
               <FacebookIcon />
             </ListItemIcon>
-            <ListItemText primary="Facebook" />
+            <ListItemText secondary="Facebook" />
           </ListItem>
           <ListItem button>
             <ListItemIcon>
               <InstagramIcon />
             </ListItemIcon>
-            <ListItemText primary="Instagram" />
+            <ListItemText secondary="Instagram" />
           </ListItem>
           <ListItem button>
             <ListItemIcon>
               <YouTubeIcon />
             </ListItemIcon>
-            <ListItemText primary="YouTube" />
+            <ListItemText secondary="YouTube" />
           </ListItem>
         </List>
       </Grid>
--- resources/js/components/public/Home.js
+++ resources/js/components/public/Home.js
@@ -3,26 +3,34 @@ import { Grid } from '@material-ui/core'
 import { Footer } from './Footer';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import Typography from '@material-ui/core/Typography'
 import stickyNotes from '../../../images/sticky-notes.jpg';
 
 function Home() {
   return (
     <Grid container style={{ marginTop: 40 }}>
-      <Grid item xs={2}></Grid>
-      <Grid container justify="center" xs={7}>
-        <h2 style={{ marginBottom: 20 }}>What's being said about the restaurants in your area?</h2>
-        <h5>
+      <Grid xs={1}></Grid>
+      <Grid xs={6} style={{ marginTop: 50, marginBottom: 20 }}>
+        <Typography variant="h3" paragraph={true}>
+          What's being said about the restaurants in your area?
+        </Typography>
+        <Typography variant="h6" color="secondary" paragraph={true}>
           This is a real-world example SPA, a React GUI interacting with a Laravel API with the following features: ACL (access control list),
           JWT authentication, CRUD implementation, REST API, Redux, Data-driven tests.
-        </h5>
-        <Grid container justify="center" style={{ margin: 20 }}>
-          <img src={stickyNotes} alt="Warthog logo" style={{ marginTop: 15, maxWidth: 400 }}/>
-        </Grid>
-        <Grid container justify="center">
-          <Button variant="contained" color="primary" component={Link} to={'/login'}>Find out now!</Button>
-        </Grid>
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to={'/login'}
+        >
+          Find out now!
+        </Button>
       </Grid>
-      <Grid item xs={2}></Grid>
+      <Grid item xs={4}>
+        <img src={stickyNotes} alt="Warthog logo" style={{ marginTop: 15, maxWidth: 400 }}/>
+      </Grid>
+      <Grid xs={1}></Grid>
       <hr />
       <Footer />
     </Grid>
