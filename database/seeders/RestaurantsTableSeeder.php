da738613e030a21da4a03ecbef141cda40925c8e
programarivm <info@programarivm.com>
2020-10-23 16:57:34 +0100

    Laravel 8 upgrade, seeder and factory namespaces

--- database/seeders/RestaurantsTableSeeder.php
+++ database/seeders/RestaurantsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Restaurant;
 use Illuminate\Database\Seeder;
 
--- database/seeders/ReviewsTableSeeder.php
+++ database/seeders/ReviewsTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use App\Restaurant;
 use App\Review;
 use App\User;
